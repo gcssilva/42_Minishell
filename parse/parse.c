342c7ee9a903d614150d08fa3ac976b6492a4bd3
gsilva <gustavocssilva1997@gmail.com>
2023-10-30 13:58:30 +0000

    30/10

--- parse/parse.c
+++ parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: gsilva <gsilva@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 14:47:01 by gsilva            #+#    #+#             */
-/*   Updated: 2023/10/24 15:29:00 by gsilva           ###   ########.fr       */
+/*   Updated: 2023/10/24 16:23:01 by gsilva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,51 @@ int	n_cmds(char	*input)
 	return (cmds);
 }
 
+int	check_quotes(char *input)
+{
+	int	i;
+	int	sflag;
+	int	dflag;
+
+	i = 0;
+	sflag = 0;
+	dflag = 0;
+	if (input[0] == '\'')
+	{
+		sflag = 1;
+	}
+	else if (input[0] == '"')
+	{
+		dflag = 1;
+	}
+	while(input[++i])
+	{
+		if (input[i] == '\'')
+		{
+			if (dflag == 0 && input[i - 1] != '\\')
+			{
+				if (sflag == 0)
+					sflag = 1;
+				else
+					sflag = 0;
+			}
+		}
+		else if (input[i] == '"')
+		{
+			if (sflag == 0 && input[i - 1] != '\\')
+			{
+				if (dflag == 0)
+					dflag = 1;
+				else
+					dflag = 0;
+			}
+		}
+	}
+	if (sflag != 0 || (dflag != 0))
+		return (0);
+	return (1);
+}
+
 char	*cjoin(char *str, char c)
 {
 	char	*new_str;
@@ -63,6 +108,20 @@ char	*cjoin(char *str, char c)
 	return(new_str);
 }
 
+void	create_struct(char *input)
+{
+	int	cmds;
+	
+	cmds = n_cmds(input);
+	data()->cmds = (char **)malloc(sizeof(char *) * (cmds + 1));
+	data()->args = (char **)malloc(sizeof(char *) * (cmds + 1));
+	while (cmds >= 0)
+	{
+		data()->cmds[cmds] = 0;
+		data()->args[cmds] = 0;
+	}
+}
+
 void	parse_input(char *input)
 {
 	int		i;
@@ -74,13 +133,13 @@ void	parse_input(char *input)
 	c = -1;
 	a = -1;
 	cmd = 0;
-	data()->cmds = (char **)malloc(sizeof(char *) * (n_cmds(input) + 1));
-	data()->args = (char **)malloc(sizeof(char *) * (n_cmds(input) + 1));
+	if (!check_quotes(input))
+		return ;
 	while (input[++i])
 	{
-		while((input[i] >= 9 && input[i] <= 13) || input[i] == 32)
+		while(input[i] && ((input[i] >= 9 && input[i] <= 13) || input[i] == 32))
 			i++;
-		while ((input[i] < 9 || input[i] > 13) && input[i] != 32)
+		while (input[i] && ((input[i] < 9 || input[i] > 13) && input[i] != 32))
 		{
 			if (input[i] == '|')
 				break ;
@@ -109,7 +168,11 @@ void	parse_input(char *input)
 		free(cmd);
 		cmd = 0;
 		if (input[i] == '|')
+		{
+			i++;
+			data()->args[++a] = ft_strdup("");
 			break ;
+		}
 		while((input[i] >= 9 && input[i] <= 13) || input[i] == 32)
 			i++;
 		while ((input[i] < 9 || input[i] > 13) && input[i] != 32)
